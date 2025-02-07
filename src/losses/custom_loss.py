a752e0186d5598ee73cf2d42a6ab361d266ac6ba
gw.wangshuo <gw.wangshuo@gmail.com>
2025-02-07 10:03:41 +0800

    Initial commit

--- src/losses/custom_loss.py
+++ src/losses/custom_loss.py
@@ -1,14 +1,10 @@
 from __future__ import annotations
 
 import warnings
-from collections.abc import Callable, Sequence
-from typing import Optional
+from collections.abc import Callable
 
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from monai.losses.focal_loss import sigmoid_focal_loss, softmax_focal_loss
 from monai.networks import one_hot
 from monai.utils import DiceCEReduction, LossReduction, look_up_option, pytorch_after
 from torch.nn.modules.loss import _Loss
@@ -74,9 +70,13 @@ class TverskyLoss(_Loss):
 
         super().__init__(reduction=LossReduction(reduction).value)
         if other_act is not None and not callable(other_act):
-            raise TypeError(f"other_act must be None or callable but is {type(other_act).__name__}.")
+            raise TypeError(
+                f"other_act must be None or callable but is {type(other_act).__name__}."
+            )
         if int(sigmoid) + int(softmax) + int(other_act is not None) > 1:
-            raise ValueError("Incompatible values: more than 1 of [sigmoid=True, softmax=True, other_act is not None].")
+            raise ValueError(
+                "Incompatible values: more than 1 of [sigmoid=True, softmax=True, other_act is not None]."
+            )
         self.include_background = include_background
         self.to_onehot_y = to_onehot_y
         self.sigmoid = sigmoid
@@ -120,12 +120,14 @@ class TverskyLoss(_Loss):
 
         if not self.include_background:
             if n_pred_ch == 1:
-                warnings.warn("single channel prediction, `include_background=False` ignored.")
+                warnings.warn(
+                    "single channel prediction, `include_background=False` ignored."
+                )
             else:
                 # if skipping background, removing first channel
                 target = target[:, 1:]
                 input = input[:, 1:]
-                
+
         if self.ignore_index is not None:
             if 0 <= self.ignore_index < n_pred_ch:
                 # add ingore_index
@@ -138,7 +140,9 @@ class TverskyLoss(_Loss):
                 )
 
         if target.shape != input.shape:
-            raise AssertionError(f"ground truth has differing shape ({target.shape}) from input ({input.shape})")
+            raise AssertionError(
+                f"ground truth has differing shape ({target.shape}) from input ({input.shape})"
+            )
 
         p0 = input
         p1 = 1 - p0
@@ -165,223 +169,9 @@ class TverskyLoss(_Loss):
             return score  # returns [N, num_classes] losses
         if self.reduction == LossReduction.MEAN.value:
             return torch.mean(score)
-        raise ValueError(f'Unsupported reduction: {self.reduction}, available options are ["mean", "sum", "none"].')
-
-
-
-class DiceLoss(_Loss):
-    """
-    Compute average Dice loss between two tensors. It can support both multi-classes and multi-labels tasks.
-    The data `input` (BNHW[D] where N is number of classes) is compared with ground truth `target` (BNHW[D]).
-
-    Note that axis N of `input` is expected to be logits or probabilities for each class, if passing logits as input,
-    must set `sigmoid=True` or `softmax=True`, or specifying `other_act`. And the same axis of `target`
-    can be 1 or N (one-hot format).
-
-    The `smooth_nr` and `smooth_dr` parameters are values added to the intersection and union components of
-    the inter-over-union calculation to smooth results respectively, these values should be small.
-
-    The original paper: Milletari, F. et. al. (2016) V-Net: Fully Convolutional Neural Networks forVolumetric
-    Medical Image Segmentation, 3DV, 2016.
-
-    """
-
-    def __init__(
-        self,
-        include_background: bool = True,
-        to_onehot_y: bool = False,
-        sigmoid: bool = False,
-        softmax: bool = False,
-        other_act: Callable | None = None,
-        squared_pred: bool = False,
-        jaccard: bool = False,
-        reduction: LossReduction | str = LossReduction.MEAN,
-        smooth_nr: float = 1e-5,
-        smooth_dr: float = 1e-5,
-        batch: bool = False,
-        weight: Sequence[float] | float | int | torch.Tensor | None = None,
-        ignore_index: int | None = None,  # add ingore_index
-    ) -> None:
-        """
-        Args:
-            include_background: if False, channel index 0 (background category) is excluded from the calculation.
-                if the non-background segmentations are small compared to the total image size they can get overwhelmed
-                by the signal from the background so excluding it in such cases helps convergence.
-            to_onehot_y: whether to convert the ``target`` into the one-hot format,
-                using the number of classes inferred from `input` (``input.shape[1]``). Defaults to False.
-            sigmoid: if True, apply a sigmoid function to the prediction.
-            softmax: if True, apply a softmax function to the prediction.
-            other_act: callable function to execute other activation layers, Defaults to ``None``. for example:
-                ``other_act = torch.tanh``.
-            squared_pred: use squared versions of targets and predictions in the denominator or not.
-            jaccard: compute Jaccard Index (soft IoU) instead of dice or not.
-            reduction: {``"none"``, ``"mean"``, ``"sum"``}
-                Specifies the reduction to apply to the output. Defaults to ``"mean"``.
-
-                - ``"none"``: no reduction will be applied.
-                - ``"mean"``: the sum of the output will be divided by the number of elements in the output.
-                - ``"sum"``: the output will be summed.
-
-            smooth_nr: a small constant added to the numerator to avoid zero.
-            smooth_dr: a small constant added to the denominator to avoid nan.
-            batch: whether to sum the intersection and union areas over the batch dimension before the dividing.
-                Defaults to False, a Dice loss value is computed independently from each item in the batch
-                before any `reduction`.
-            weight: weights to apply to the voxels of each class. If None no weights are applied.
-                The input can be a single value (same weight for all classes), a sequence of values (the length
-                of the sequence should be the same as the number of classes. If not ``include_background``,
-                the number of classes should not include the background category class 0).
-                The value/values should be no less than 0. Defaults to None.
-
-        Raises:
-            TypeError: When ``other_act`` is not an ``Optional[Callable]``.
-            ValueError: When more than 1 of [``sigmoid=True``, ``softmax=True``, ``other_act is not None``].
-                Incompatible values.
-
-        """
-        super().__init__(reduction=LossReduction(reduction).value)
-        if other_act is not None and not callable(other_act):
-            raise TypeError(f"other_act must be None or callable but is {type(other_act).__name__}.")
-        if int(sigmoid) + int(softmax) + int(other_act is not None) > 1:
-            raise ValueError("Incompatible values: more than 1 of [sigmoid=True, softmax=True, other_act is not None].")
-        self.include_background = include_background
-        self.to_onehot_y = to_onehot_y
-        self.sigmoid = sigmoid
-        self.softmax = softmax
-        self.other_act = other_act
-        self.squared_pred = squared_pred
-        self.jaccard = jaccard
-        self.smooth_nr = float(smooth_nr)
-        self.smooth_dr = float(smooth_dr)
-        self.batch = batch
-        weight = torch.as_tensor(weight) if weight is not None else None
-        self.register_buffer("class_weight", weight)
-        self.weight = weight
-        self.class_weight: None | torch.Tensor
-        self.ignore_index = ignore_index  # add ingore_index
-
-    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        """
-        Args:
-            input: the shape should be BNH[WD], where N is the number of classes.
-            target: the shape should be BNH[WD] or B1H[WD], where N is the number of classes.
-
-        Raises:
-            AssertionError: When input and target (after one hot transform if set)
-                have different shapes.
-            ValueError: When ``self.reduction`` is not one of ["mean", "sum", "none"].
-
-        Example:
-            >>> from monai.losses.dice import *  # NOQA
-            >>> import torch
-            >>> from monai.losses.dice import DiceLoss
-            >>> B, C, H, W = 7, 5, 3, 2
-            >>> input = torch.rand(B, C, H, W)
-            >>> target_idx = torch.randint(low=0, high=C - 1, size=(B, H, W)).long()
-            >>> target = one_hot(target_idx[:, None, ...], num_classes=C)
-            >>> self = DiceLoss(reduction='none')
-            >>> loss = self(input, target)
-            >>> assert np.broadcast_shapes(loss.shape, input.shape) == input.shape
-        """
-        if self.sigmoid:
-            input = torch.sigmoid(input)
-
-        n_pred_ch = input.shape[1]
-        if self.softmax:
-            if n_pred_ch == 1:
-                warnings.warn("single channel prediction, `softmax=True` ignored.")
-            else:
-                input = torch.softmax(input, 1)
-
-        if self.other_act is not None:
-            input = self.other_act(input)
-
-        if self.to_onehot_y:
-            if n_pred_ch == 1:
-                warnings.warn("single channel prediction, `to_onehot_y=True` ignored.")
-            else:
-                target = one_hot(target, num_classes=n_pred_ch)
-
-        if not self.include_background:
-            if n_pred_ch == 1:
-                warnings.warn("single channel prediction, `include_background=False` ignored.")
-            else:
-                # if skipping background, removing first channel
-                target = target[:, 1:]
-                input = input[:, 1:]
-                
-        if self.ignore_index is not None:
-            if 0 <= self.ignore_index < n_pred_ch:
-                # add ingore_index
-                mask = [i for i in range(n_pred_ch) if i != self.ignore_index]
-                input = input[:, mask]
-                target = target[:, mask]
-                
-                if self.class_weight is not None:
-                    self.class_weight = self.weight[mask].to(self.class_weight.device)
-            else:
-                warnings.warn(
-                    f"ignore_index {self.ignore_index} is out of range. Valid range: [0, {n_pred_ch - 1}]. Ignoring."
-                )
-        else:
-            mask = [i for i in range(n_pred_ch)]
-
-        if target.shape != input.shape:
-            raise AssertionError(f"ground truth has different shape ({target.shape}) from input ({input.shape})")
-
-        # reducing only spatial dimensions (not batch nor channels)
-        reduce_axis: list[int] = torch.arange(2, len(input.shape)).tolist()
-        if self.batch:
-            # reducing spatial dimensions and batch
-            reduce_axis = [0] + reduce_axis
-
-        intersection = torch.sum(target * input, dim=reduce_axis)
-
-        if self.squared_pred:
-            ground_o = torch.sum(target**2, dim=reduce_axis)
-            pred_o = torch.sum(input**2, dim=reduce_axis)
-        else:
-            ground_o = torch.sum(target, dim=reduce_axis)
-            pred_o = torch.sum(input, dim=reduce_axis)
-
-        denominator = ground_o + pred_o
-
-        if self.jaccard:
-            denominator = 2.0 * (denominator - intersection)
-
-        f: torch.Tensor = 1.0 - (2.0 * intersection + self.smooth_nr) / (denominator + self.smooth_dr)
-
-        num_of_classes = target.shape[1]
-        if self.class_weight is not None and num_of_classes != 1:
-            # make sure the lengths of weights are equal to the number of classes
-            if self.class_weight.ndim == 0:
-                self.class_weight = torch.as_tensor([self.class_weight] * num_of_classes)
-            else:
-                if self.class_weight.shape[0] != num_of_classes:
-                    raise ValueError(
-                        """the length of the `weight` sequence should be the same as the number of classes.
-                        If `include_background=False`, the weight should not include
-                        the background category class 0."""
-                    )
-            if self.class_weight.min() < 0:
-                raise ValueError("the value/values of the `weight` should be no less than 0.")
-            # apply class_weight to loss
-            f = f * self.class_weight.to(f)
-
-        if self.reduction == LossReduction.MEAN.value:
-            f = torch.mean(f)  # the batch and channel average
-        elif self.reduction == LossReduction.SUM.value:
-            f = torch.sum(f)  # sum over the batch and channel dims
-        elif self.reduction == LossReduction.NONE.value:
-            # If we are not computing voxelwise loss components at least
-            # make sure a none reduction maintains a broadcastable shape
-            broadcast_shape = list(f.shape[0:2]) + [1] * (len(input.shape) - 2)
-            f = f.view(broadcast_shape)
-        else:
-            raise ValueError(f'Unsupported reduction: {self.reduction}, available options are ["mean", "sum", "none"].')
-
-        return f
-
+        raise ValueError(
+            f'Unsupported reduction: {self.reduction}, available options are ["mean", "sum", "none"].'
+        )
 
 
 class CELoss(_Loss):
@@ -395,13 +185,18 @@ class CELoss(_Loss):
     ) -> None:
         super().__init__()
         reduction = look_up_option(reduction, DiceCEReduction).value
-        
+
         if pytorch_after(1, 10):
             self.cross_entropy = nn.CrossEntropyLoss(
-                weight=weight, reduction=reduction, label_smoothing=label_smoothing, ignore_index=ignore_index
+                weight=weight,
+                reduction=reduction,
+                label_smoothing=label_smoothing,
+                ignore_index=ignore_index,
             )
         else:
-            self.cross_entropy = nn.CrossEntropyLoss(weight=weight, reduction=reduction, ignore_index=ignore_index)
+            self.cross_entropy = nn.CrossEntropyLoss(
+                weight=weight, reduction=reduction, ignore_index=ignore_index
+            )
 
         self.old_pt_ver = not pytorch_after(1, 10)
 
@@ -426,7 +221,7 @@ class CELoss(_Loss):
             target = target.to(dtype=input.dtype)
 
         return self.cross_entropy(input, target)  # type: ignore[no-any-return]
-    
+
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
 
         if input.dim() != target.dim():
@@ -442,50 +237,53 @@ class CELoss(_Loss):
                 f"got shape {input.shape} and {target.shape}."
             )
 
-        ce_loss = self.ce(input, target) if input.shape[1] != 1 else self.bce(input, target)
-        
+        ce_loss = (
+            self.ce(input, target) if input.shape[1] != 1 else self.bce(input, target)
+        )
+
         return ce_loss
 
 
 class CustomLoss(nn.Module):
     def __init__(
-        self, 
+        self,
         alpha=0.4,
         beta=0.6,
-        tversky_coef=1.0, 
-        ce_coef=1.0, 
+        tversky_coef=1.0,
+        ce_coef=1.0,
         ignore_index=2,
-        weight=None
+        weight=None,
     ):
         super().__init__()
         self.tversky_coef = tversky_coef
         self.ce_coef = ce_coef
         self.ignore_index = ignore_index
         self.weight = torch.tensor(weight) if weight is not None else None
-        
+
         self.tversky_loss = TverskyLoss(
-            alpha=alpha, beta=beta,
+            alpha=alpha,
+            beta=beta,
             ignore_index=ignore_index,
-            include_background=True, to_onehot_y=True, softmax=True
+            include_background=True,
+            to_onehot_y=True,
+            softmax=True,
         )
 
-        self.ce_loss = CELoss(
-                weight=self.weight,
-                ignore_index=ignore_index
-            )
-        
+        self.ce_loss = CELoss(weight=self.weight, ignore_index=ignore_index)
 
     def forward(self, preds, targets):
         loss_dict = {}
         loss = 0.0
 
         if self.tversky_coef > 0:
-            loss_dict["tversky_loss"] = self.tversky_coef * self.tversky_loss(preds, targets)
+            loss_dict["tversky_loss"] = self.tversky_coef * self.tversky_loss(
+                preds, targets
+            )
             loss += loss_dict["tversky_loss"]
 
         if self.ce_coef > 0:
             loss_dict["ce_loss"] = self.ce_coef * self.ce_loss(preds, targets)
             loss += loss_dict["ce_loss"]
-            
+
         loss_dict["loss"] = loss
         return loss_dict
