02c996a81349cd4caf94a33fd1693465ff91c8b7
gw.wangshuo <gw.wangshuo@gmail.com>
2025-02-07 14:14:22 +0800

    Initial commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@ set -e
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "@        Download and Preprocess the Data          @"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-python3 src/utils/prepare_data.py
+# python3 src/utils/prepare_data.py
 python3 src/utils/generate_segmentation_mask.py
 
 
@@ -37,6 +37,6 @@ valid_ids=(
 
 for vid in "${valid_ids[@]}"; do
     for config in "${configs[@]}"; do
-        python src/train.py --config "src/config/$config" --valid_id "$vid"
+        python3 src/train.py --config "src/config/$config" --valid_id "$vid"
     done
 done
